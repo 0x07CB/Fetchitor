292884ae22daad513acb6ec9b98820d8b2e96d40
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-07-08 14:48:21 +0200

    Update and rename info_connction.py to info_connection.py

---
+++ FETCHITOR/sensors_codes/info_connection.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#coding: utf-8
+
+from os import system
+import hashlib
+
+scripts_ = """
+echo "$(date) - USER: $USER - at origin of this running scripts. is $(whoami) the user is confirmed without verification(automatic)." >> /var/log/info_checking_connection_history.log
+echo "[Checking connected users]" >> /var/log/info_checking_connection_history.log
+echo "$(w && who)" >> /var/log/info_checking_connection_history.log
+echo "" >> /var/log/info_checking_connection_history.log
+"""
+
+def curiosity_func_who_are_logged_in():
+  if loglevel:
+    system("{script}".format(
+        script = scripts_
+    ))
+    
+  
