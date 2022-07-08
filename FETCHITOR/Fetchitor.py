8c0be7ea941d1e965702076ade9e61918f3b2a8b
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-07-08 11:08:44 +0000

    seeds 2

---
+++ FETCHITOR/Fetchitor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+#coding: utf-8
+
+# libs
+
+
+
+# args parsing 
+
+#USAGE: 
+#       fetchitor [OPTIONS] [SUB-COMMAND]
+#
+#
+# SUB-COMMAND:
+#       INIT
+#       UPDATE
+#       RELOAD
+#       CONFIG
+#       RUN
+#       STOP
+#       VERIFY
+#       ENABLE
+#       DISABLE
+# 
+# OPTIONS:
+#       -f / --file-out       
+#       -m / --menu-config 
+#       -c / --config-file
+#       --get-cpu-temp --get-cpu-usage
+#       --refresh-time
+#       --get-ram-usage
+#       --get-entropy-available
+#       --get-date  --get-time
+#       --get-users-connected
+#       --get-connections-stats
+#       --get-status-systemd
+
+
