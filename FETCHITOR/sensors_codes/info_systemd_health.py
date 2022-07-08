53b35e5a4b9b95f88b40fc21eeb81680592927e5
0x07CB <83157348+0x07CB@users.noreply.github.com>
2022-07-08 13:56:40 +0200

    Update info_systemd_health.py

--- FETCHITOR/sensors_codes/info_systemd_health.py
+++ FETCHITOR/sensors_codes/info_systemd_health.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
 #coding: utf-8
 #
-def  
+
+
+
+
+class systemdHealthChecker(objects):
+  def __init__(self,):
+    pass
+  
+  def detect_failure_status(self):
+    pass
+  
+  def notify_local(self):
+    pass
+  
+  def notification_sending_over_SSH(self):
+    pass
+  
+  def notification_sending_over_TCP(self):
+    pass
+  
+  def get(self,json_out=None ,csv_out=None):
+    pass
